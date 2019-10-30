ff03d8107deadb4e61720ee419f57bba90017dba
Eze-IO <kryptowashere@gmail.com>
2019-10-30 14:11:29 -0500

    Set default route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :date_of_infections
   resources :animals
   resources :owners
+  root "animals#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
